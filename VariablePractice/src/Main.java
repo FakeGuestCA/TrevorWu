72b3016cdd128d0f0aa445a79e836680daf798a8
FakeGuestCA <94019187+FakeGuestCA@users.noreply.github.com>
2021-11-12 14:09:22 -0800

    Add files via upload

---
+++ VariablePractice/src/Main.java
@@ -0,0 +1,45 @@
+public class Main
+{
+    public static void main( String[] args )
+    {
+
+        // Problem 2 //
+        int room;
+        double e;
+
+        room = 113;
+        e = 2.71828;
+        String c = "Computer Science";
+        System.out.println("This is room # "+room);
+        System.out.println("e is close to "+e);
+        System.out.println("I am learning a bit about "+c);
+
+        // Your Schedule//
+        String c1 = "English III";
+        String t1 = "Ms. Lapan";
+        String c2 = "Precalculus";
+        String t2 = "Mrs. Gideon";
+        String c3 = "Music Theory";
+        String t3 = "Mr. Davis";
+        String c4 = "Biotechnology";
+        String t4 = "Ms. Palmer";
+        String c5 = "Principles of Technology I";
+        String t5 = "Ms. Garcia";
+        String c6 = "Latin II";
+        String t6 = "Mrs. Barnett";
+        String c7 = "AP US History";
+        String t7 = "Ms. Johannessen";
+        String c8 = "Business Computer Information Systems";
+        String t8 = "Mr. James";
+        System.out.println("+--------------------------------------------------------------+");
+        System.out.println("| 1 |                            "+c1+" |       "+t1+" |");
+        System.out.println("| 2 |                            "+c2+" |     "+t2+" |");
+        System.out.println("| 3 |                           "+c3+" |       "+t3+" |");
+        System.out.println("| 4 |                          "+c4+" |      "+t4+" |");
+        System.out.println("| 5 |             "+c5+" |      "+t5+" |");
+        System.out.println("| 6 |                               "+c6+" |    "+t6+" |");
+        System.out.println("| 7 |                          "+c7+" | "+t7+" |");
+        System.out.println("| 8 |  "+c8+" |       "+t8+" |");
+        System.out.println("+--------------------------------------------------------------+");
+    }
+}
