0ba6a980cb613d4bb360f162b6319664bf2732b1
FakeGuestCA <94019187+FakeGuestCA@users.noreply.github.com>
2022-01-18 14:14:16 -0800

    Add files via upload

---
+++ DataAbstractionAssignment/TestCases/CustomerTest.java
@@ -0,0 +1,20 @@
+import org.junit.Before;
+import org.junit.Test;
+import java.util.ArrayList;
+import java.util.Date;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class CustomerTest {
+    Customer test;
+
+    @Before
+    public void setup(){
+        test = new Customer();
+    }
+
+    @Test
+    public void testwithdraw(){
+        assertEquals();
+    }
+}
